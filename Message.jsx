668367279eaf33168ef27d55a383034489b29480
Pranav Kulkarni <52783889+Pranav00747@users.noreply.github.com>
2023-01-02 22:42:53 +0530

    Update Message.jsx

--- Message.jsx
+++ Message.jsx
@@ -3,7 +3,7 @@ import './Message.css';
 
 function Message(props)
 {
- return(<div class="msg"><img class="msgimg" src={photo}/><h4 class="tname">{name}/><p class="main_tweet_chat">{tweet_chat}</p></div>);
+ return(<div class="msg"><img class="msgimg" src={props.photo}/><h4 class="tname">{props.name}/><p class="main_tweet_chat">{props.tweet_chat}</p></div>);
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(<Message photo="Pranav.png" name="Pranav" tweet_chat="I love sooo much Anjali !."/>);
